e0e1f60c7cb27790fbe1bd0fc9fe20cbe3df775c
Lippsxd <158731525+Lippsxd@users.noreply.github.com>
2024-02-03 16:44:51 +0530

    Add files via upload

---
+++ LippsMusic/plugins/Logs.py
@@ -0,0 +1,49 @@
+'''from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from config import LOG_ID
+from LippsMusic import app
+
+
+async def new_message(chat_id: int, message: str, reply_markup=None):
+    await app.send_message(chat_id=chat_id, text=message, reply_markup=reply_markup)
+
+@app.on_message(filters.new_chat_members)
+async def on_new_chat_members(client: Client, message: Message):
+    if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
+        added_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        title = message.chat.title
+        username = f"@{message.chat.username}"
+        chat_id = message.chat.id
+        fallen = f"**✫** <b><u>ɴᴇᴡ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {username}\n**ᴄʜᴀᴛ ᴛɪᴛʟᴇ :** {title}\n\n**ᴀᴅᴅᴇᴅ ʙʏ :** {added_by}"
+        reply_markup = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(
+            message.from_user.first_name,
+            user_id=message.from_user.id
+        )
+    ]
+])
+
+        
+        await new_message(LOG_ID, fallen, reply_markup)
+
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(client: Client, message: Message):
+    if (await client.get_me()).id == message.left_chat_member.id:
+        remove_by = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        title = message.chat.title
+        username = f"@{message.chat.username}"
+        chat_id = message.chat.id
+        goodbye = f"**✫** <b><u>ʟᴇғᴛ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ** : {username}\n**ᴄʜᴀᴛ ᴛɪᴛʟᴇ :** {title}\n\n**ʀᴇᴍᴏᴠᴇᴅ ʙʏ :** {remove_by}"
+        reply_markup = InlineKeyboardMarkup([
+    [
+        InlineKeyboardButton(
+            message.from_user.first_name,
+            user_id=message.from_user.id
+        )
+    ]
+])
+
+        
+        await new_message(LOG_ID, goodbye, reply_markup)
+'''
