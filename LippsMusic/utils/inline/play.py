133ce53f0ad677c53b9aec6475595f3beb1ca7d6
Lippsxd <158731525+Lippsxd@users.noreply.github.com>
2024-02-04 14:12:44 +0530

    Update play.py

--- LippsMusic/utils/inline/play.py
+++ LippsMusic/utils/inline/play.py
@@ -62,6 +62,7 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),            
         ],
