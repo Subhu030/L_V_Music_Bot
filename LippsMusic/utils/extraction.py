11d96c8e8f5eed99f2d480ce264c87d5197e6bbe
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 12:24:58 +0530

    Update extraction.py

--- LippsMusic/utils/extraction.py
+++ LippsMusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from HarleyMusic import app
+from LippsMusic import app
 
 
 async def extract_user(m: Message) -> User:
