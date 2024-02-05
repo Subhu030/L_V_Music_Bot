526720ece0f0c7feaead53fddb0fae39d274821a
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 12:43:46 +0530

    Update stream.py

--- LippsMusic/utils/stream/stream.py
+++ LippsMusic/utils/stream/stream.py
@@ -9,7 +9,7 @@ from LippsMusic import Carbon, YouTube, app
 from LippsMusic.core.call import Lipps
 from LippsMusic.misc import db
 from LippsMusic.utils.database import add_active_video_chat, is_active_chat
-from HarleyMusic.utils.exceptions import AssistantErr
+from LippsMusic.utils.exceptions import AssistantErr
 from LippsMusic.utils.inline import aq_markup, close_markup, stream_markup
 from LippsMusic.utils.pastebin import LippsBin
 from LippsMusic.utils.stream.queue import put_queue, put_queue_index
