349b2ff948d8f8fc90b65edbffc3f932d967e862
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 12:30:51 +0530

    Update queue.py

--- LippsMusic/utils/stream/queue.py
+++ LippsMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from HarleyMusic.misc import db
-from HarleyMusic.utils.formatters import check_duration, seconds_to_min
+from LippsMusic.misc import db
+from LippsMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
