7c0d420d07b4d2b9a67ed7b2292620b8e6d6b3ef
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 12:28:52 +0530

    Update admins.py

--- LippsMusic/utils/decorators/admins.py
+++ LippsMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from HarleyMusic import app
-from HarleyMusic.misc import SUDOERS, db
-from HarleyMusic.utils.database import (
+from LippsMusic import app
+from LippsMusic.misc import SUDOERS, db
+from LippsMusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
