6c3c287f0cdfb1f159164db22512d416d8d855c9
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 15:46:10 +0530

    Update thumbnails.py

--- LippsMusic/utils/thumbnails.py
+++ LippsMusic/utils/thumbnails.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic import app
+from LippsMusic import app
 from config import YOUTUBE_IMG_URL
 
 
@@ -71,8 +71,8 @@ async def get_thumb(videoid):
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.5)
         draw = ImageDraw.Draw(background)
-        arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("AnonXMusic/assets/font.ttf", 30)
+        arial = ImageFont.truetype("LippsMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("LippsMusic/assets/font.ttf", 30)
         draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
             (55, 560),
