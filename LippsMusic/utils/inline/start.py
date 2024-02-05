7f9200f2059be6afbaf31ac164ed7c6b19909a20
Lippsxd <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 07:58:54 +0530

    Update start.py

--- LippsMusic/utils/inline/start.py
+++ LippsMusic/utils/inline/start.py
@@ -28,7 +28,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
            InlineKeyboardButton(text=_["S_B_7"], url=f"https://github.com/Lippsxd/LippsMusicBot"),
