42383d3330d19b3c836a5541cef4df037e0af66d
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-05 12:33:06 +0530

    Update help.py

--- LippsMusic/utils/inline/help.py
+++ LippsMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from HarleyMusic import app
+from LippsMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
