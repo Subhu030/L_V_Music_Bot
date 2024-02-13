5f1121966da6f1d2cacf6cddbc3ce34717428f93
Lipps <158731525+Lippsxd@users.noreply.github.com>
2024-02-13 22:10:48 +0530

    Update help.py

--- LippsMusic/utils/inline/help.py
+++ LippsMusic/utils/inline/help.py
@@ -25,16 +25,18 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
                 ),
-            ],
-            [
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_5"],
                     callback_data="help_callback hb5",
@@ -53,16 +55,18 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_8"],
                     callback_data="help_callback hb8",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_9"],
                     callback_data="help_callback hb9",
                 ),
-            ],
-            [
                 InlineKeyboardButton(
                     text=_["H_B_10"],
                     callback_data="help_callback hb10",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_11"],
                     callback_data="help_callback hb11",
@@ -81,6 +85,8 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_14"],
                     callback_data="help_callback hb14",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_15"],
                     callback_data="help_callback hb15",
