f3cf984850e9aaf2dc9e4d1b87da7b59d8fb5e86
Roblex <149115379+Subhu030@users.noreply.github.com>
2024-03-05 03:17:38 +0530

    Update start.py

--- LippsMusic/plugins/bot/start.py
+++ LippsMusic/plugins/bot/start.py
@@ -33,7 +33,7 @@ async def start_pm(client, message: Message, _):
         if name[0:4] == "help":
             keyboard = help_pannel(_)
             return await message.reply_photo(
-                photo=config.START_IMG_URL,
+                photo=config.https://graph.org/file/1188e06d7a0bf19d01e5e.jpg,
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
             )
@@ -85,7 +85,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_photo(
-            photo=config.START_IMG_URL,
+            photo=config.https://graph.org/file/1188e06d7a0bf19d01e5e.jpg,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
@@ -102,7 +102,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        photo=config.START_IMG_URL,
+        photo=config.https://graph.org/file/1188e06d7a0bf19d01e5e.jpg,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
@@ -137,7 +137,7 @@ async def welcome(client, message: Message):
 
                 out = start_panel(_)
                 await message.reply_photo(
-                    photo=config.START_IMG_URL,
+                    photo=config.https://graph.org/file/1188e06d7a0bf19d01e5e.jpg,
                     caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
